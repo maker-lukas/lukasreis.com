1a14433b94f944c54c6e7d9b188963f1f9013039
Lukas <itslukasreis@gmail.com>
2026-05-16 21:06:55 +0100

    add new projects + add vibesock to stats

---
+++ src/content/blog/print.md
@@ -0,0 +1,7 @@
+---
+title: "I let the internet print message on my home printer"
+description: "Why i made print.lukasreis.com"
+date: 2026-04-26
+tags: ["Web", "Hardware", "Printer"]
+---
+
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,36 +3,49 @@ export const projects = [
     title: "40%ish Split Keyboard",
     description: "A custom build Split Keyboard.",
     image: "/images/keyboard.jpg",
-    github: "https://github.com/maker-lukas/40-ish-split-keyboard",
+    link: "https://github.com/maker-lukas/40-ish-split-keyboard",
   },
   {
     title: "print.lukasreis.com",
     description: "A website where you can leave a small message and it will auto-print on my home printer!",
     image: "/images/print.png",
-    github: "https://print.lukasreis.com"
+    link: "https://print.lukasreis.com"
   },
   {
     title: "B.E.T.T",
     description: "Better Enchanted Tool Textures. A Minecraft Resouce pack that changes tool textures depending on the enchantments",
     image: "/images/pack.png",
-    github: "https://github.com/maker-lukas/B.E.T.T",
+    link: "https://github.com/maker-lukas/B.E.T.T",
   },
   {
     title: "Dial",
     description: "A smooth scrolling 70mm dial with as AS5600 sensor.",
     image: "/images/dial.avif",
-    github: "https://github.com/maker-lukas/scroll-dial",
+    link: "https://github.com/maker-lukas/scroll-dial",
   },
   {
     title: "FireFly Tiny Whoop",
     description: "A custom 75mm tiny whoop drone frame",
     image: "/images/firefly.png",
-    github: "https://github.com/maker-lukas/firefly-drone"
+    link: "https://github.com/maker-lukas/firefly-drone"
   },
   {
     title: "Spotify Overlay",
     description: "A minimal, always-on-top Spotify overlay for Linux.",
     image: "/images/spotify-overlay.png",
-    github: "https://github.com/maker-lukas/spotify-overlay"
+    link: "https://github.com/maker-lukas/spotify-overlay"
+  },
+  {
+    title: "Scroll bindings",
+    description: "Bind scroll wheel up and down to any keybind in Minecraft.",
+    image: "/images/scroll_bindings.png",
+    link: "https://modrinth.com/mod/scroll-bindings"
+  },
+  {
+    title: "Always On Locator Bar",
+    description: "A small mod that stops the locator bar (waypoint dots) from disappearing whenever the XP bar pops up to animate absorbed experience.",
+    image: "/images/aolb.png",
+    link: "https://modrinth.com/mod/always-on-locator-bar",
+    pixelated: true
   }
 ];
